2e6262970225b41c2bff2930bf25c8545d08cb81
Wendy Zamora <wendys.zamora@gmail.com>
2025-10-15 18:20:10 -0300

    act4

---
+++ actividad4/mi-proyecto/src/app/components/PokemonList.jsx
@@ -0,0 +1,53 @@
+"use client"; // para que ejecute del lado del clinete
+import { useEffect, useState } from "react";
+import axios from "axios";
+import PokemonItem from "./PokemonItem";
+
+export default function PokemonList() {
+    const [ContPokemons, setPokemons] = useState([]);
+
+    useEffect(() => { //trae los datos de la API cuando se ejecuta el componnete
+        //accion a ejecutar
+        async function fetchPokemons() { // Defino func asinc porque useEffect no puede ser asinc directamenete
+            try {
+                const respuesta = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20"); // espero la respuesta de la API
+                setPokemons(respuesta.data.results); // actualizo el estado con los pokemons
+            } 
+            catch (error) {
+                console.error("Error al traer los pokemons", error);
+            }
+        }
+
+        fetchPokemons(); // llamo a la funcion para que se ejecute
+
+        /*return() => {
+            //Cleanup opcional
+        };*/
+
+    }, []); // [dependencia]--> el array vacio es para que se ejecute una sola vez al montar el componente
+
+    return (
+        <div> 
+            <h2>Listado de Pokemons</h2>
+
+            {ContPokemons.length === 0 ? ( // muestra "cargando.." si no hay pokemons
+                <p>Cargando...</p>
+            ) : ( // si hay, los muestro en una lista
+                <ul
+                     style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    padding: 0,
+                    justifyContent: "center",
+                    }}
+                >
+                    {ContPokemons.map((pokemon) => ( // recorro el array de pokemons
+                        //<li key={pokemon.name}> {pokemon.name} </li> // key es para que react identifique cada elemento - muestro el nombre
+                        <PokemonItem key={pokemon.name} name_pokemon={pokemon.name} /> // uso el componente PokemonItem para mostrar cada pokemon
+                    
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
---
+++ actividad4/mi-proyecto/src/app/page.tsx
@@ -0,0 +1,10 @@
+import PokemonList from "./components/PokemonList";
+
+export default function Home() {
+  return (
+    <main> 
+      <h1>Pokedex</h1>
+      <PokemonList />
+    </main>
+  );
+}
